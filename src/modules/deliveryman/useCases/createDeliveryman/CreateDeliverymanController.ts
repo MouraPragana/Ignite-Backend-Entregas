edd60a0a7d5c97f00e950a491179636d3ba0378a
Mateus Pragana <mateuspragana@gmail.com>
2022-02-04 21:22:56 -0300

    feat: criando deliverymanController e seu useCase

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+
+import { CreateDeliverymanUseCase } from "./CreateDeliverymanUseCase";
+
+export class CreateDeliverymanController {
+  async handle(request: Request, response: Response) {
+    const createDeliverymanUseCase = new CreateDeliverymanUseCase();
+
+    const { username, password } = request.body;
+
+    const result = await createDeliverymanUseCase.execute({
+      username,
+      password,
+    });
+
+    return response.status(201).json(result);
+  }
+}
