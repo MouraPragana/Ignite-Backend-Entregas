a75f454368f4066f28251ba01d4204ddd71558c2
Mateus Pragana <mateuspragana@gmail.com>
2022-02-05 12:21:41 -0300

    refactor: criando rotas de autenticação de deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,23 @@ import { Router } from "express";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientControler";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController =
+  new AuthenticateDeliverymanController();
 
 routes.post("/client/", createClientController.handle);
 routes.post("/deliveryman/", createDeliverymanController.handle);
-routes.post("/authenticate", authenticateClientController.handle);
+
+routes.post("/client/authenticate", authenticateClientController.handle);
+routes.post(
+  "/deliveryman/authenticate",
+  authenticateDeliverymanController.handle
+);
 
 export { routes };
